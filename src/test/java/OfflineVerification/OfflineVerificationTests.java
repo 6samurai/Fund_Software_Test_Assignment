40f0acc74e5745326dd587422575696288357191
6samurai <christzammit93@gmail.com>
2018-11-09 12:22:03 +0100

    Completed tests for OfflineVerification: 	- LuhnCheck
    					- PrefixAndCardType

--- src/test/java/OfflineVerification/OfflineVerificationTests.java
+++ src/test/java/OfflineVerification/OfflineVerificationTests.java
@@ -20,18 +20,125 @@ public class OfflineVerificationTests {
 
     }
 
-    public CCInfo createValidCCInfo(){
-        return new CCInfo("Chris","222,Test","VISA","371449635398431","11/4/2020","Test");
-
-    }
-
     @Test
     public void testVerifyOperationLuhnCheck(){
-        ccInfo = createValidCCInfo();
+        ccInfo = new CCInfo("Chris","222,Test","American Express","371449635398431","11/4/2020","Test");;
         OfflineVerification offlineVerification = new OfflineVerification();
         boolean check = offlineVerification.verifyLuhn(ccInfo.getCardNumber());
 
         assertTrue(check);
     }
+
+    @Test
+    public void testVerifyOperationLuhnCheck_Blank(){
+        ccInfo = new CCInfo("Chris","222,Test","American Express","","11/4/2020","Test");;
+        OfflineVerification offlineVerification = new OfflineVerification();
+        boolean check = offlineVerification.verifyLuhn(ccInfo.getCardNumber());
+        assertFalse(check);
+    }
+
+    @Test
+    public void testVerifyOperationLuhnCheck_InvalidValue(){
+        ccInfo = new CCInfo("Chris","222,Test","American Express","2132132131312321","11/4/2020","Test");;
+        OfflineVerification offlineVerification = new OfflineVerification();
+        boolean check = offlineVerification.verifyLuhn(ccInfo.getCardNumber());
+        assertFalse(check);
+    }
+
+
+    @Test
+    public void testVerifyPrefixAndCardType_Valid_AmericanExpress(){
+        ccInfo = new CCInfo("Chris","222,Test","American Express","371449635398431","11/4/2020","Test");;
+        OfflineVerification offlineVerification = new OfflineVerification();
+        boolean check = offlineVerification.verifyPrefixAndCardType(ccInfo.getCardNumber(), ccInfo.getCardType());
+
+        assertTrue(check);
+    }
+
+    @Test
+    public void testVerifyPrefixAndCardType_Valid_MasterCard(){
+        ccInfo = new CCInfo("Chris","222,Test","Mastercard","5555555555554444","11/4/2020","Test");;
+        OfflineVerification offlineVerification = new OfflineVerification();
+        boolean check = offlineVerification.verifyPrefixAndCardType(ccInfo.getCardNumber(), ccInfo.getCardType());
+
+        assertTrue(check);
+    }
+
+    @Test
+    public void testVerifyPrefixAndCardType_Valid_VISA(){
+        ccInfo = new CCInfo("Chris","222,Test","VISA","4111111111111111","11/4/2020","Test");;
+        OfflineVerification offlineVerification = new OfflineVerification();
+        boolean check = offlineVerification.verifyPrefixAndCardType(ccInfo.getCardNumber(), ccInfo.getCardType());
+
+        assertTrue(check);
+    }
+
+
+    @Test
+    public void testVerifyPrefixAndCardType_Blank(){
+        ccInfo = new CCInfo("Chris","222,Test","American Express","0","11/4/2020","Test");;
+        OfflineVerification offlineVerification = new OfflineVerification();
+        boolean check = offlineVerification.verifyPrefixAndCardType(ccInfo.getCardNumber(), ccInfo.getCardType());
+
+        assertFalse(check);
+    }
+
+
+
+    @Test
+    public void testVerifyPrefixAndCardType_InvalidCardType_AmericanExpress(){
+        ccInfo = new CCInfo("Chris","222,Test","American Expressss","371449635398431","11/4/2020","Test");;
+        OfflineVerification offlineVerification = new OfflineVerification();
+        boolean check = offlineVerification.verifyPrefixAndCardType(ccInfo.getCardNumber(), ccInfo.getCardType());
+
+        assertFalse(check);
+    }
+    @Test
+    public void testVerifyPrefixAndCardType_InvalidCardNumber_AmericanExpress(){
+        ccInfo = new CCInfo("Chris","222,Test","American Express","571449635398431","11/4/2020","Test");;
+        OfflineVerification offlineVerification = new OfflineVerification();
+        boolean check = offlineVerification.verifyPrefixAndCardType(ccInfo.getCardNumber(), ccInfo.getCardType());
+
+        assertFalse(check);
+    }
+
+
+    @Test
+    public void testVerifyPrefixAndCardType_InvalidCardType_MasterCard(){
+        ccInfo = new CCInfo("Chris","222,Test","Mastercardddd","5555555555554444","11/4/2020","Test");;
+        OfflineVerification offlineVerification = new OfflineVerification();
+        boolean check = offlineVerification.verifyPrefixAndCardType(ccInfo.getCardNumber(), ccInfo.getCardType());
+
+        assertFalse(check);
+    }
+
+
+    @Test
+    public void testVerifyPrefixAndCardType_InvalidCardNumber_MasterCard(){
+        ccInfo = new CCInfo("Chris","222,Test","Mastercard","0555555555554444","11/4/2020","Test");;
+        OfflineVerification offlineVerification = new OfflineVerification();
+        boolean check = offlineVerification.verifyPrefixAndCardType(ccInfo.getCardNumber(), ccInfo.getCardType());
+
+        assertFalse(check);
+    }
+
+    @Test
+    public void testVerifyPrefixAndCardType_InvalidCardType_VISA(){
+        ccInfo = new CCInfo("Chris","222,Test","VISAaaa","4111111111111111","11/4/2020","Test");;
+        OfflineVerification offlineVerification = new OfflineVerification();
+        boolean check = offlineVerification.verifyPrefixAndCardType(ccInfo.getCardNumber(), ccInfo.getCardType());
+
+        assertFalse(check);
+    }
+
+    @Test
+    public void testVerifyPrefixAndCardType_InvalidCardNumber_VISA(){
+        ccInfo = new CCInfo("Chris","222,Test","VISA","1111111111111111","11/4/2020","Test");;
+        OfflineVerification offlineVerification = new OfflineVerification();
+        boolean check = offlineVerification.verifyPrefixAndCardType(ccInfo.getCardNumber(), ccInfo.getCardType());
+
+        assertFalse(check);
+    }
+
 
 }
